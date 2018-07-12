0b1333345dd0a4847f42fb187045f224507e11cf
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-07-11 20:02:07 -0700

    add json data and images

---
+++ src/game.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import warriors from "./data/nba/warriors.json";
+
+class Game extends Component {
+  state = {
+    cards= []
+  }
+
+  render () {
+    return (
+      <div>
+      
+      </div>
+    )
+  }
+}
+
+export default Game;
