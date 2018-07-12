6581e88febe749f3fa209b452eb4975b4b16317a
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-07-11 23:37:01 -0700

    map through json for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,11 @@
 import React, {Component} from "react";
 
 const Card = props => {
+  return(
   <div className="card">
-
+    <img src={props.img} alt={props.alt} className="card__img"/>
   </div>  
+  )
 }
+
+export default Card;
--- src/game.js
+++ src/game.js
@@ -1,15 +1,27 @@
 import React, {Component} from "react";
 import warriors from "./data/nba/warriors.json";
+import Card from "./components/Card";
 
 class Game extends Component {
   state = {
-    cards= []
+    cards: [...warriors]
   }
+
+componentDidMount() {
+  console.log(this.state.cards)
+}
 
   render () {
     return (
       <div>
-      
+        <div className="row game__container">
+          <div className="card__container">
+        {this.state.cards.map(x => (
+          <Card img={x.img} alt={x.name}/>
+        ))}
+        {/* <Card img={this.state.cards[0].img} alt={this.state.cards[0].name}/>  */}
+        </div>
+        </div>
       </div>
     )
   }
