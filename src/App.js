6e51263edf38254e119144a771fd67a0278774f9
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-08-12 12:44:54 -0700

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Game from "./pages/Game"
+
 
 class App extends Component {
   render() {
     return (
-     <Game/>
+      <Router>
+        <Switch>
+          <Game/>
+        </Switch>
+      </Router>  
     );
   }
 }
