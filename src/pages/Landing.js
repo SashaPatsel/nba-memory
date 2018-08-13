12d6d446b4654a5a9eb708c4992e07d8e4a24737
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-08-12 17:43:13 -0700

    create new landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Landing extends Component {
   state = {
@@ -6,12 +6,40 @@ class Landing extends Component {
   }
 
 
-  render () {
+  render() {
     return (
-      <div className="landing" >
+      <section className="landing" >
+        <h1 className="landing__heading-primary">The NBA memory game</h1>
+
+        <div className="landing__nav container">
+          <div className="row">
+            <div className="col-6">
+
+              <figcaption className="landing__img-caption">
+                Rules</figcaption>
+              <img className="landing__img" src="https://toppng.com/public/uploads/preview/whistle-11530975730zyj5qkg7xt.png" />
+
+            </div>
+
+            <div className="col-6">
+
+              <figcaption className="landing__img-caption">
+                Play</figcaption>
+              <img className="landing__img" src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Basketball.png" />
+
+            </div>
 
 
-      </div>
+            {/* <div className="col-4">
+
+              <figcaption className="landing__img-caption">
+                Rules</figcaption>
+              <img className="landing__img" src="https://toppng.com/public/uploads/preview/whistle-11530975730zyj5qkg7xt.png" />
+
+            </div> */}
+          </div>
+        </div>
+      </section>
     )
   }
 }
